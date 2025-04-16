600da7adc89435812374a221bde692c5fc17c78a
Daniel <ceballoscoraldanielfernando@gmail.com>
2025-04-16 15:12:24 -0500

    Creacion de pagina de inicio de sesion

---
+++ src/components/Login/index.js
@@ -0,0 +1,32 @@
+import Logo from "../Logo/Logo";
+import "./Login.css"
+
+function Login() {
+    return (
+        <div>
+            <Logo></Logo>
+            <main>
+                <section>
+                    <h2>Bienvenido</h2>
+                    <p>Por favor inicie sesión</p>
+                </section>
+                <div class="login-page">
+                    <div class="form">
+                        <form class="login-form">
+                            <select name="Tipo de usuario" id="Usuario">
+                                <option value="cero">Tipo de usuario</option>
+                                <option value="admin">Administrador</option>
+                                <option value="analista">Analista de inventario</option>
+                                <option value="lider">Lider de cuadrilla</option>
+                            </select>
+                            <input id="usuario" type="text" placeholder="Usuario" />
+                            <input id="contraseña" type="password" placeholder="Contraseña" />
+                            <button id="ingresar">Ingresar</button>
+                        </form>
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
+}
+export default Login
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,8 @@
+function Logo () {
+    return(
+        <div>
+            <img src="/img/SECPET-logo.png" alt="logo SECPET"></img>
+        </div>
+    );
+}
+export default Logo;
