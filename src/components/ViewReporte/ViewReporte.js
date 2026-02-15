641c871f5898f0e4fc4bcc0ed554ef930a9f31b4
Daniel <ceballoscoraldanielfernando@gmail.com>
2026-02-15 16:24:22 -0500

    Agregando texto de carga

--- src/components/ViewReporte/ViewReporte.js
+++ src/components/ViewReporte/ViewReporte.js
@@ -15,6 +15,7 @@ function ViewReporte() {
     const [usuario, actualizarUsuario] = useState({});
     const [materiales, actualizarMaterial] = useState([]);
     const data = JSON.parse(localStorage.getItem("usuario"));
+    const [cargando, actualizarCargando] = useState(true)
 
     // Obtnemos toda la información del reporte, el usuario que lo reportó y los materiales asociados al reporte
     // Se utiliza useEffect para hacer la llamada a la API cuando el componente se monta
@@ -26,6 +27,7 @@ function ViewReporte() {
 
                 actualizarReporte(response.data);
                 actualizarUsuario(response2.data);
+                actualizarCargando(false);
             } catch (error) {
                 console.log(error);
             }
@@ -43,22 +45,27 @@ function ViewReporte() {
         obtenerMaterial();
     }, []); // Dependencias vacías para que se ejecute solo una vez al montar el componente
 
+
+
     // función para formatear la fecha en un formato legible
     const formatearFecha = (fecha) => {
-        if (!fecha) return "fecha no disponible";
-        const date = new Date(fecha);
+        if (!fecha) return "Fecha no disponible";
 
-        const opciones = {
-            year: 'numeric',
-            month: '2-digit',
-            day: '2-digit',
-            hour: '2-digit',
-            minute: '2-digit',
-            hour12: true
-        };
+        const nuevaFecha = new Date(fecha);
+        const dia = String(nuevaFecha.getDate()).padStart(2, "0");
+        const mes = String(nuevaFecha.getMonth() + 1).padStart(2, "0");
+        const año = nuevaFecha.getFullYear();
 
-        // Formatear la fecha en el formato "dd/mm/yyyy hh:mm AM/PM"
-        return new Intl.DateTimeFormat('es-CO', opciones).format(date);
+        let horas = nuevaFecha.getHours();
+        const minutos = String(nuevaFecha.getMinutes()).padStart(2, "0");
+
+        // Determinamos si es AM o PM
+        const ampm = horas >= 12 ? 'PM' : 'AM';
+        horas = horas % 12;
+        horas = horas ? horas : 12;
+        const horasStr = String(horas).padStart(2, "0");
+
+        return `${dia}/${mes}/${año} ${horasStr}:${minutos} ${ampm}`;
     };
 
     // Determinar la ruta a la cual se devuelve dependiendo del estado de aprobación del reporte y el tipo de usuario
@@ -89,69 +96,72 @@ function ViewReporte() {
         <div style={{ marginBottom: "100px" }}>
             <Header volver={ruta}></Header>
             <div className="contendor">
-                <h3>Reporte número: {idReporte}</h3>
-                <h3>Número de OT: {reporte.NumOT}</h3>
-                <h3>Reportado por: {usuario.Nombre} {usuario.Apellido}</h3>
-                <h3>Fecha de reporte: {formatearFecha(reporte.FechaReporte)}</h3>
-                <h3>Fecha de cierre OT: {formatearFecha(reporte.FechaCierreOT)}</h3>
-                <div className="material">
-                    <table id="tablaMateriales">
-                        <thead>
-                            <th>Código de material</th>
-                            <th>Cantidad</th>
-                        </thead>
-                        {materiales.length > 0 ? (
-                            //Mostrar en una tabla los materiales
-                            <tbody>
-                                {materiales.map((material, index) => (
-                                    <tr key={index}>
-                                        <td>{material.CodigoMaterial}</td>
-                                        <td>{material.CantMaterial}</td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        )
-                            : (
-                                <tr>
-                                    <td colSpan="2">No hay materiales registrados para este reporte.</td>
-                                </tr>
-                            )
-
-                        }
-
-                    </table>
-
-                </div>
-                {reporte.Aprobacion === null ? ( // Si el reporte no ha sido aprobado o rechazado, se muestra el formulario para agregar un comentario y aprobar o rechazar
+                {cargando ? (<p>Cargando Reporte...</p>) : (
                     <div>
-                        <h3>Agregar comentario:</h3>
-                        <textarea id="comentario" value={comentario} onChange={(e) => actualizarComentario(e.target.value)}></textarea>
-                        <div id="aprobacion">
-                            <button id="aprobado" onClick={() => aprobacion("Aprobado")}>Aprobar</button>
-                            <button id="rechazado" onClick={() => aprobacion("No aprobado")}>Rechazar</button>
+                        <h3>Reporte número: {idReporte}</h3>
+                        <h3>Número de OT: {reporte.NumOT}</h3>
+                        <h3>Reportado por: {usuario.Nombre} {usuario.Apellido}</h3>
+                        <h3>Fecha de reporte: {formatearFecha(reporte.FechaReporte)}</h3>
+                        <h3>Fecha de cierre OT: {formatearFecha(reporte.FechaCierreOT)}</h3>
+                        <div className="material">
+                            <table id="tablaMateriales">
+                                <thead>
+                                    <th>Código de material</th>
+                                    <th>Cantidad</th>
+                                </thead>
+                                {materiales.length > 0 ? (
+                                    //Mostrar en una tabla los materiales
+                                    <tbody>
+                                        {materiales.map((material, index) => (
+                                            <tr key={index}>
+                                                <td>{material.CodigoMaterial}</td>
+                                                <td>{material.CantMaterial}</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                )
+                                    : (
+                                        <tr>
+                                            <td colSpan="2">No hay materiales registrados para este reporte.</td>
+                                        </tr>
+                                    )
+
+                                }
+
+                            </table>
+
                         </div>
-                    </div>
-                ) : reporte.Aprobacion === "Aprobado" ? ( // Si el reporte ha sido aprobado/rechazado, se muestra el comentario del analista y el estado de aprobación
-                    <div>
-                        <div>
-                            <h3>Comentario del Analista:</h3>
-                            <p>{reporte.Comentario}</p>
-                        </div>
-                        <div className="check" style={{ backgroundColor: "#4bdb51" }}>
-                            <h2>{reporte.Aprobacion}</h2>
-                        </div>
-                    </div>
-                ) : (
-                    <div>
-                        <div>
-                            <h3>Comentario del Analista:</h3>
-                            <p>{reporte.Comentario}</p>
-                        </div>
-                        <div className="check" style={{ backgroundColor: "#db4b4b" }}>
-                            <h2>{reporte.Aprobacion}</h2>
-                        </div>
-                    </div>
-                )}
+                        {reporte.Aprobacion === null ? ( // Si el reporte no ha sido aprobado o rechazado, se muestra el formulario para agregar un comentario y aprobar o rechazar
+                            <div>
+                                <h3>Agregar comentario:</h3>
+                                <textarea id="comentario" value={comentario} onChange={(e) => actualizarComentario(e.target.value)}></textarea>
+                                <div id="aprobacion">
+                                    <button id="aprobado" onClick={() => aprobacion("Aprobado")}>Aprobar</button>
+                                    <button id="rechazado" onClick={() => aprobacion("No aprobado")}>Rechazar</button>
+                                </div>
+                            </div>
+                        ) : reporte.Aprobacion === "Aprobado" ? ( // Si el reporte ha sido aprobado/rechazado, se muestra el comentario del analista y el estado de aprobación
+                            <div>
+                                <div>
+                                    <h3>Comentario del Analista:</h3>
+                                    <p>{reporte.Comentario}</p>
+                                </div>
+                                <div className="check" style={{ backgroundColor: "#4bdb51" }}>
+                                    <h2>{reporte.Aprobacion}</h2>
+                                </div>
+                            </div>
+                        ) : (
+                            <div>
+                                <div>
+                                    <h3>Comentario del Analista:</h3>
+                                    <p>{reporte.Comentario}</p>
+                                </div>
+                                <div className="check" style={{ backgroundColor: "#db4b4b" }}>
+                                    <h2>{reporte.Aprobacion}</h2>
+                                </div>
+                            </div>
+                        )}
+                    </div>)}
             </div>
             <Footer></Footer>
         </div>
